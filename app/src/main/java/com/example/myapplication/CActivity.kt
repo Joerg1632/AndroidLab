3d1d6a71da8f08d55a4b21febc5b34d0843a4eb9
y.vegner <y.vegner@g.nsu.ru>
2025-10-20 12:04:36 +0700

    addLog

--- app/src/main/java/com/example/myapplication/CActivity.kt
+++ app/src/main/java/com/example/myapplication/CActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,12 +10,15 @@ class CActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_c)
+        Log.d("Lifecycle", "CActivity onCreate")
 
         findViewById<Button>(R.id.buttonNextC).setOnClickListener {
+            Log.d("Navigation", "Переход с C на A")
             startActivity(Intent(this, MainActivity::class.java))
         }
 
         findViewById<Button>(R.id.buttonBackC).setOnClickListener {
+            Log.d("Lifecycle", "Переход с C на B")
             finish()
         }
     }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,12 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("Lifecycle", "MainActivity onCreate")
+
         findViewById<Button>(R.id.buttonNextA).setOnClickListener {
+            Log.d("Navigation", "Переход с A на B")
             val intent= Intent(this@MainActivity, BActivity::class.java)
             startActivity(intent)
         }
 
         findViewById<Button>(R.id.buttonExitA).setOnClickListener {
+            Log.d("Lifecycle", "Finish app")
             finishAffinity()
         }
     }
