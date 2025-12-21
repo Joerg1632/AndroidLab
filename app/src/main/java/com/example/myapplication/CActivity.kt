e1fe3a4c17ecca7e8fa3f4f5a67c926c74fb8338
Yuri Wegner <131969860+Joerg1632@users.noreply.github.com>
2025-12-22 00:55:23 +0700

    Update CActivity.kt

--- app/src/main/java/com/example/myapplication/CActivity.kt
+++ app/src/main/java/com/example/myapplication/CActivity.kt
@@ -7,19 +7,16 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_c)
-        Log.d("Lifecycle", "CActivity onCreate")
+
+        Log.d("Lifecycle", "C onCreate")
 
         findViewById<Button>(R.id.buttonNextC).setOnClickListener {
-            Log.d("Navigation", "Переход с C на A")
+            Log.d("Navigation", "C -> A")
             startActivity(Intent(this, MainActivity::class.java))
-        }
-
-        findViewById<Button>(R.id.buttonBackC).setOnClickListener {
-            Log.d("Lifecycle", "Переход с C на B")
-            finish()
         }
     }
 }
